822fe846332bb3e368759d90b5d69b0130808f29
Prabu <prabu.rajan@gmail.com>
2015-05-22 09:35:59 +0530

    implemented filtering on client side using angular filters

--- controllers/candidatesController.js
+++ controllers/candidatesController.js
@@ -2,17 +2,12 @@ hireZen.controller('CandidatesController', function($scope, CandidatesStore) {
 	$scope.candidates = CandidatesStore.getCandidates();
 
 	$scope.teams = CandidatesStore.getTeams();
-	$scope.managers = CandidatesStore.getManagers();
 	$scope.statuses = CandidatesStore.getStatuses();
 
 	$scope.selection = CandidatesStore.getSelection();
 
 	$scope.select = function(candidate) {
-		CandidatesStore.getSelection().candidate = candidate;
-	};
-
-	$scope.filterManager = function(manager) {
-		CandidatesStore.setSelectedManager(manager);
+		CandidatesStore.setSelectedCandidate(candidate);
 	};
 
 	$scope.filterTeam = function(team) {
--- stores/candidatesStore.js
+++ stores/candidatesStore.js
@@ -3,59 +3,74 @@
 hireZen.factory('CandidatesStore', function() {
 	var candidates = [{
 		name: "Naresh Kumar",
-		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015"
+		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015",
+		team : "Composer"
 	}, {
 		name: "Dinesh Karthik",
-		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015"
+		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015",
+		team : "Workspace"
 	}, {
 		name: "Zaheer Khan",
-		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015"
+		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015",
+		team : "Foundation Services"
 	}, {
 		name: "Shikhar Dawan",
-		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015"
+		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015",
+		team: "Human Workflow"
 	}, {
 		name: "Virender Sehwag",
-		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015"
+		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015",
+		team: "Cloud OPS"
 	}, {
 		name: "Andrea",
-		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015"
+		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015",
+		team: "Workspace"
 	}, {
 		name: "Sridevi",
-		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015"
+		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015",
+		team: "Foundation Services"
 	}, {
 		name: "Salman Khan",
-		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015"
+		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015",
+		team: "Composer"
 	}, {
 		name: "Aamir Khan",
-		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015"
+		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015",
+		team: "Human Workflow"
 	}, {
 		name: "Ranbhir Kapoor",
-		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015"
+		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015",
+		team: "Workspace"
 	}, {
 		name: "Virath Kohli",
-		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015"
+		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015",
+		team: "Composer"
 	}, {
 		name: "MS Dhoni",
-		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015"
+		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015",
+		team: "Foundation Services"
 	}, {
 		name: "Trisha",
-		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015"
+		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015",
+		team: "Cloud OPS"
 	}, {
 		name: "Michael Jackson",
-		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015"
+		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015",
+		team: "Cloud OPS"
 	}, {
 		name: "Narendra Modi",
-		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015"
+		description: "Skills : HTML, CSS, Angular, Java  Applied : 20 Mar, 2015",
+		team: "Human Workflow"
 	}];
 
+
+
 	var teams = ["All", "Workspace", "Composer", "Foundation Services", "Human Workflow", "Cloud OPS"];
-	var managers = ["All", "Prabu", "Magesh", "Mahesh Rao", "Sri Krishan", "Kapil", "Krishna"];
 	var statuses = ["All", "In Progress", "Offer Approval", "Offered", "Rejected"];
 
 	var selection = {
-        team : "All",
-        manager : "All",
-        status : "All",
+        team : "",
+        status : "",
 		candidate : candidates[0]
     };
 
@@ -66,9 +81,6 @@ hireZen.factory('CandidatesStore', function() {
 		},
 		getTeams: function() {
 			return teams;
-		},
-		getManagers: function() {
-			return managers;
 		},
 		getStatuses: function() {
 			return statuses;
@@ -82,20 +94,20 @@ hireZen.factory('CandidatesStore', function() {
             return selection.team;
         },
 
-        getSelectedManager : function() {
-            return selection.manager;
-        },
-
         getSelection : function() {
             return selection;
         },
 
+		setSelectedCandidate : function (candidate) {
+			selection.candidate = candidate == "All" ? "" : candidate;
+		},
+
         setSelectedTeam : function(team) {
-            selection.team = team;
+            selection.team = team == "All" ? "" : team;
         },
 
-        setSelectedManager : function(manager) {
-            selection.manager = manager;
-        }
+		getCandidate : function(id) {
+			return candidates[id];
+		}
 	};
 });
